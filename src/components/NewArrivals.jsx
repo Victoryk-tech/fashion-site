f0914a976645980a1eb8554faac7f4c40543c848
VictoryK-tech <kemelevictory3802@gmail.com>
2024-05-25 06:33:50 +0100

    add

--- src/components/NewArrivals.jsx
+++ src/components/NewArrivals.jsx
@@ -34,7 +34,7 @@ export const NewArrivals = () => {
       <div className="w-full text-center text-[34px] font-bold">
         <h1>New Collections</h1>
       </div>
-      <div className="grid grid-cols-3 items-center justify-center px-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center px-8">
         {newArrivs.map((data, index) => {
           return (
             <div
