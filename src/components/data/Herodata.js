00a4a3a9565dc407be6ad22c2bda689f650d49de
VictoryK-tech <kemelevictory3802@gmail.com>
2024-05-26 08:36:55 +0100

    shopping

--- src/components/data/Herodata.js
+++ src/components/data/Herodata.js
@@ -5,23 +5,23 @@ import bag from "../../assets/bagCart.png";
 export const heroData = [
   {
     id: 1,
-    title: "Elevate your ",
-    title2: "Style",
-    description: "Find the best fashion style for you",
+    title: "Shop ",
+    title2: "exclusively",
+    description: "living life affordably",
     image: shopgirl,
   },
   {
     id: 2,
-    title: "Perfect outfit ",
-    title2: "sale",
-    description: "Find the best fashion style for you",
+    title: "Perfect shopping ",
+    title2: "site",
+    description: "shop from the best",
     image: shopbag,
   },
   {
     id: 3,
-    title: "Browse the ",
-    title2: "Lookbook",
-    description: "Find the best fashion style for you",
+    title: "Magnificiently sorted to your ",
+    title2: "taste",
+    description: "Find the best item for you",
     image: bag,
   },
 ];
